d63fbcf9ed3b98808326ad5d6ea5763d00638a96
jasid <sidjack972@gmail.com>
2020-04-19 12:25:54 +0200

    Creation des application Web Pocker Manager Ajout du tableau Joueurs

--- appweb/models.py
+++ appweb/models.py
@@ -6,12 +6,18 @@ COLOR_JETONS = [('RED', 'Rouge'), ('BLUE', 'Bleu'), ('GREEN', 'Vert'), ('BLACK',
 
 
 # Create your models here.
+# Tableua de
+class Joueurs(models.Model):
+    nombreJoueur = models.IntegerField(max_length=2,)
+
+# Tableau Jetons
 class Jetons(models.Model):
-    color = models.CharField(choices=COLOR_JETONS,max_length=25)
+    color = models.CharField(choices=COLOR_JETONS, max_length=25)
     value = models.IntegerField()
     nombre_jeton = models.IntegerField()
 
 
+# Tebleau Blinds
 class Blinds(models.Model):
     times = models.TimeField()
     smallBlind = models.IntegerField()
